721a65c74e9cfdb7dba2a0fdc35336573e6b86ed
Lucas Uchôa da Veiga <lucas.veiga@laveiga.com.br>
2025-02-24 11:46:16 -0300

    Execution application

---
+++ src/main/java/luveiga/com/SpringDataRest/SpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package luveiga.com.SpringDataRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationa
+public class SpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestApplication.class, args);
+	}
+}
