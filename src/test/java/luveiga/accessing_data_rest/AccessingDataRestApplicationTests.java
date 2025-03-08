dc88b8a3ea19a4efbd29a15d1db9b3d7a0da359e
Lucas Uchôa da Veiga <lucas.veiga@laveiga.com.br>
2025-03-08 01:18:19 -0300

    🚀 init → Adding base structure

---
+++ src/test/java/luveiga/accessing_data_rest/AccessingDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package luveiga.accessing_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
